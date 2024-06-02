3cd0b69134d80d06ac1b74320933cf51b98ce88b
Parminder <84897471+parminder911@users.noreply.github.com>
2024-06-02 12:02:31 +0530

    Add files via upload
    Program for Private constructor , we cannot call constructor from another class , because its have Private Access modifier . other class cannot access private constructor . 
    you can make Static method , bcz static method call without object , direct call by class name .

---
+++ privateconstructor.java
@@ -0,0 +1,20 @@
+public class privateconstructor {
+    int a; float ft;
+
+private privateconstructor()
+{
+a= 50;
+ft=80.60F;
+
+    System.out.println(
+            a + " "+ " " + ft
+    );
+
+}
+    public static void main(String[] args)
+    {
+    privateconstructor parminder = new privateconstructor();
+
+    }
+
+}
