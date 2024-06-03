95edff517fe324895f06a8d839e4818034199016
Parminder <84897471+parminder911@users.noreply.github.com>
2024-06-03 07:36:05 +0530

    thiskeyword.java

--- thiskeyword.java
+++ thiskeyword.java
@@ -1,22 +1,30 @@
 public class thiskeyword {
     int a;  //instance variable
+    thiskeyword()
+    {
+        this(200); // call parametrized constructor
+
+        System.out.println("constructor");
+
+
+    }
 
     thiskeyword(int a)
     {
-     
+//     this();      // use to call default constructor
         this.a=a;  //local and instance var. are same
           }
     void  show()
     {
-        System.out.println(this);
+        System.out.println("print this keyword reference => " +this);
         System.out.println(a);
     }
 
     //refer current object = instance variable ,
     // both object and this keyword show same reference number
     public static void main(String[] args) {
-        thiskeyword parminder = new thiskeyword(500);
-        System.out.println(parminder);
+        thiskeyword parminder = new thiskeyword();
+        System.out.println( "print object reference => " +parminder);
 
         parminder.show();
 
@@ -25,3 +33,4 @@ public class thiskeyword {
 
     }
 }
+
